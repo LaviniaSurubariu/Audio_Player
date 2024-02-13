89c33f6ce931b3d80077003dd5995eb4c760d9dc
LaviniaSurubariu <surubariu.lavinia@gmail.com>
2024-02-13 22:03:39 +0200

    little format tweaks

--- lib/main.dart
+++ lib/main.dart
@@ -25,57 +25,59 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final AudioPlayer player = AudioPlayer();
-  final List<String> listCuvinte = <String>['salut', 'ma numesc', 'cum esti?', 'sunt bine'];
+  final List<String> words = <String>['salut', 'ma numesc', 'cum esti?', 'sunt bine'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-          title: const Text('Basic Phrases'),
-          titleTextStyle: const TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-          ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: const Text('Audio Player'),
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 24,
         ),
-        body: GridView.builder(
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(5),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: 8,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
             padding: const EdgeInsets.all(5),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemCount: 8,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(5),
-                child: GestureDetector(
-                  onTap: () {
-                    player.seek(Duration.zero);
-                    player.play(AssetSource('0${index + 1}.mp3'));
-                  },
-                  child: Expanded(
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          gradient: LinearGradient(colors: <Color>[
-                            Colors.blue,
-                            Colors.blueAccent,
-                            Colors.lightBlue,
-                            Colors.lightBlueAccent,
-                          ])),
-                      child: SizedBox(
-                        height: 300,
-                        child: Center(
-                          child: Text(
-                              index.isEven
-                                  ? listCuvinte.elementAt((index / 2).floor())
-                                  : '${listCuvinte.elementAt((index / 2).floor())} (Germana)',
-                              style: const TextStyle(fontSize: 20, color: Colors.black),
-                              textAlign: TextAlign.center),
-                        ),
-                      ),
+            child: GestureDetector(
+              onTap: () {
+                player.seek(Duration.zero);
+                player.play(AssetSource('0${index + 1}.mp3'));
+              },
+              child: Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      gradient: LinearGradient(colors: <Color>[
+                        Colors.blue,
+                        Colors.blueAccent,
+                        Colors.lightBlue,
+                        Colors.lightBlueAccent,
+                      ])),
+                  child: SizedBox(
+                    height: 300,
+                    child: Center(
+                      child: Text(
+                          index.isEven
+                              ? words.elementAt((index / 2).floor())
+                              : '${words.elementAt((index / 2).floor())} (Germana)',
+                          style: const TextStyle(fontSize: 20, color: Colors.black),
+                          textAlign: TextAlign.center),
                     ),
                   ),
                 ),
-              );
-            }));
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
